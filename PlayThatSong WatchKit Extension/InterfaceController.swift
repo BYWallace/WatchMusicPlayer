7231d541b05bd76b3953c8da184d3d0eb65570b0
Brett Wallace <bwallace90@gmail.com>
2015-09-08 22:42:50 -0400

    Initial setup of connection between WatchKit and iOS

--- PlayThatSong WatchKit Extension/InterfaceController.swift
+++ PlayThatSong WatchKit Extension/InterfaceController.swift
@@ -38,5 +38,10 @@ class InterfaceController: WKInterfaceController {
     }
     
     @IBAction func playSongButtonPressed() {
+        var info = ["Key": "Value"]
+        
+        WKInterfaceController.openParentApplication(info, reply: { (reply, error) -> Void in
+            println("reply: \(reply) error: \(error)")
+        })
     }
 }
